2cf4e6c307d741513523c39f5b39ffa5ebe6fe0b
Nguyen Thanh Vu <thanhvuttq@gmail.com>
2024-01-21 19:04:42 +0700

    video 31

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const configViewEngine = require("./config/viewEngine");
 const wedRoutes = require("./routes/wed");
-const mysql = require("mysql2");
+const connection = require("./config/dataBase");
 const { table } = require("console");
 const { name } = require("ejs");
 const app = express();
@@ -17,14 +17,6 @@ configViewEngine(app);
 app.use("/", wedRoutes);
 
 //test connection
-// Create a MySQL database connection
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: "3307", //default:3306
-  user: "root", //default: emty
-  password: "123456",
-  database: "hoidanit",
-});
 
 //simple queiry
 connection.query("SELECT * FROM Users u", function (err, results, fields) {
